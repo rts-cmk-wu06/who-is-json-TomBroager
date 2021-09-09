2fdb3f1716396b78be9c87bf784859e2e03b026d
TomBroager <U773553@rts-365.dk>
2021-09-09 14:47:43 +0200

    add solution

--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
 let teamMember = {
-    "member": {
+    "member": [{
         "firstname": "Tom",
         "lastname": "Broager",
         "age": 52,
         "haircolor": "blond",
         "shoesize": 43
-    }
+    },
+    {
+        "firstname": "Jørgen",
+        "lastname": "Hansen",
+        "age": 55,
+        "haircolor": "dark",
+        "shoesize": 45
+    },
+    {
+        "firstname": "Hans",
+        "lastname": "Jensen",
+        "age": 70,
+        "haircolor": "rød",
+        "shoesize": 29
+    },
+    {
+        "firstname": "Ole",
+        "lastname": "Gokke",
+        "age": 51,
+        "haircolor": "Grøn",
+        "shoesize": 45
+    }]
 }
 
-console.log(teamMember.member.firstname);
+teamMember.member.forEach(element => console.log(element.firstname));
