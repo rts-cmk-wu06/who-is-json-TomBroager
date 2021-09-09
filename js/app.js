4a1827bb6f264600df73c6347e2610a9bec12859
TomBroager <U773553@rts-365.dk>
2021-09-09 14:11:15 +0200

    add solution to app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let teamMember = {
+    "member": {
+        "firstname": "Tom",
+        "lastname": "Broager",
+        "age": 52,
+        "haircolor": "blond",
+        "shoesize": 43
+    }
+}
+
+console.log(teamMember.member.firstname);
