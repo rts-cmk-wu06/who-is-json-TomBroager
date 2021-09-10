8d28b6c9f6cf979b1195fbb7c7fe7ea418371e36
TomBroager <U773553@rts-365.dk>
2021-09-10 13:20:32 +0200

    add solution

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ let list = document.querySelector('#person-data');
 /* fetch promises basic */
 fetch('./team.json')
     .then((response) => response.json())
-    .then((responseJsonData) => {
-        responseJsonData.member.forEach((element) => {
+    .then((jsonData) => {
+        jsonData.member.forEach((element) => {
             let listItem = document.createElement('li');
             listItem.textContent = `${element.firstname} ${element.lastname + ' |'} ${'age: ' + element.age + ' |'} ${'haircolor: ' + element.haircolor + ' |'} ${'shoesize: ' + element.shoesize}`;
             list.appendChild(listItem);
